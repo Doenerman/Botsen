b3550b12d1d985e2aa89d63a6bbf9dcee21c9cf3
doenerman <jan.uthoff@rwth-aachen.de>
2021-01-17 13:06:48 +0100

    Rewrote commands.py

--- sample/commands.py
+++ sample/commands.py
@@ -1,51 +1,75 @@
 # commands.py
 import discord
+import enum
 
-async def print_commands(client: discord.Client, channel: discord.Message.channel):
-    """The method sends a message with all supported commands the bot can receive over
-    the text chat and react properly.
+class Commands:
+    def __init__(self, client: discord.Client):
+        self.client = client
 
-    The method iterates over the dictionary 'command_dict' and adds each
-    the key of entry to the text that will be send to 'channel'.
-    """
-    msg_content = str()
-    msg_content += "The following commands are supported:\n"
-    for key in Commands.command_dict:
-        msg_content += "!%s "%(key)
-    msg_content += "\n\nFor descriptions for a command type:\n"
-    msg_content += "\t!help <command>"
+    async def print_commands(self,
+                             message: discord.Message):
+        """The method sends a message with all supported commands the bot can receive over
+        the text chat and react properly.
 
-    await channel.send( msg_content )
+        The method iterates over the dictionary 'command_dict' and adds each
+        the key of entry to the text that will be send to 'channel'.
 
-async def print_help(client: discord.Client,
-                     message: discord.Message,
-                     command: str):
-    """The method sends a help message for the given command on the text
-    channel of 'message'.The method sends a help message for the given
-    command on the text channel of 'message'.
+        Parameters:
+        -----------
+            client: discordClient
+                The discord client that shall print the command message into
+                'channel'.
+            channel: discord.Message.channel
+                The text channel, where the information string shall be send to.
+        """
+        
+        msg_content = 'I don\'t know that command, but i can\'t help you either'
+        #msg_content += "The following commands are supported:\n"
+        #for key in self.command_dict:
+        #    msg_content += "!%s "%(key)
+        #msg_content += "\n\nFor descriptions for a command type:\n"
+        #msg_content += "\t!help <command>"
 
-    The method checks if the given string 'command' is a key of the
-    dictionary 'command_dict'. If so, the description of that command is
-    send to the textchannel over which the Message 'message' was received
-    over. In case the command is not supported, a list of supported commands
-    is send to the text channel.
-    """
-    if command in Commands.command_dict:
-        await message.channel.send( Commands.command_dict[command][1] )
-    else:
-        await message.channel.send( "The command {command} is not supported" )
-        await Command.print_commands( message )
+        await message.channel.send( msg_content )
 
-async def shutdown(client: discord.Client, _):
-    """ Shuts down the given Discord Client.
-    """
-    await client.close()
-    print('Client shutting down')
+    async def print_help(self,
+                         channel: discord.Message.channel,
+                         command: str):
+        """The method sends a help message for the given command on the text
+        channel of 'message'.The method sends a help message for the given
+        command on the text channel of 'message'.
 
+        The method checks if the given string 'command' is a key of the
+        dictionary 'command_dict'. If so, the description of that command is
+        send to the textchannel over which the Message 'message' was received
+        over. In case the command is not supported, a list of supported commands
+        is send to the text channel.
 
-command_dict = {
-    "shutdown" : 
-    [ shutdown, "Shuts down the bot" ],
-    "commands" : 
-    [ print_commands, "Prints this message" ],
-}
+        Parameters:
+        -----------
+            client: discord.Client
+                The discord client that shall print the information message about
+                the commands.
+            channel: discord.Message.channel
+                The text channel, where the information string shall be send to.
+            command:
+                The string of the command whose more detailed description is
+                requested.
+        """
+        if command in command_dict:
+            await channel.send( command_dict[command][1] )
+        else:
+            await channel.send( "The command {command} is not supported" )
+            await self.print_commands( message )
+
+    async def shutdown(self,
+                       message: discord.Message):
+        """ Shuts down the given Discord Client.
+
+            Parameters:
+            -----------
+                client: discord.Client
+                    The client to shut down.
+        """
+        await self.client.close()
+        print('Client shutting down')
